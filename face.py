53e5b15b6a57242960473d7e374c39623927b266
cyril.ruedin <cyril.ruedin@he-arc.ch>
2017-04-06 10:54:58 +0200

    VISAGITZAZATOR

--- face.py
+++ face.py
@@ -36,25 +36,36 @@ for (x, y, w, h) in faces:
 
 cv2.imshow("Faces found", image)
 cv2.waitKey(0)
+
+
 for facx in face:
     offset = 200;
     img = facx
-    
+
+    img2 = img
+    cv2.imshow("img2", img2)
+    cv2.waitKey(0)
+
     ret,thresh = cv2.threshold(img,127,255,0)
-    th2 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,41,18)
+    th2 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,41,8)
+    cv2.imshow("2", th2)
+    cv2.waitKey(0)
     im2, contours, hierarchy = cv2.findContours(th2,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     cv2.drawContours(img, contours, -1, (0,255,0), 3)
 
+
     hsv = cv2.cvtColor(fond, cv2.COLOR_BGR2HSV)
-    
+
+    # cv2.imshow("3", th2)
+    # cv2.waitKey(0)
+
     height, width = img.shape
     out = np.zeros((height,width,3), np.uint8)
     for i in range(height):
         for j in range(width):
-            out[i,j] = hsv[offset+i, offset+j] if img[i,j] > 3 else [hsv[offset+i,offset+j][0],hsv[offset+i,offset+j][1], hsv[offset+i,offset+j][2]*0.5]#[img[i,j], img[i,j], img[i,j]] 
+            out[i,j] = [hsv[offset+i,offset+j][0],hsv[offset+i,offset+j][1], hsv[offset+i,offset+j][2]+img2[i,j]*0.26]
+            #out[i,j] = hsv[offset+i, offset+j] if th2[i,j] < 3 else [hsv[offset+i,offset+j][0],hsv[offset+i,offset+j][1], hsv[offset+i,offset+j][2]*0.5]#[img[i,j], img[i,j], img[i,j]]
     #img[0:height, 0:width] = fond[offset:height+offset, offset:width+offset] - img[0:height, 0:width]
     out = cv2.cvtColor(out, cv2.COLOR_HSV2BGR)
     cv2.imshow("trellele", out)
     cv2.waitKey(0)
-
-
